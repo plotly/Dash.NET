a75eac6ee30e475f0653e8aa2f05fc30c9ac39dd
Kevin Schneider <schneider.kev@outlook.de>
2021-02-02 16:52:49 +0100

    Add ccustom operators and the CallbackBinding type

---
+++ src/Dash.NET/CallbackBinding.fs
@@ -0,0 +1,17 @@
+﻿namespace Dash.NET
+
+type CallbackResultBinding = {
+    Target: Dependency // Dependency
+    BoxedResult: obj
+} with
+    static member create (target:Dependency) (boxedResult:obj) =
+        {
+            Target = target
+            BoxedResult = boxedResult
+        }
+
+    static member inline bindResult (target:Dependency) (callbackResult) = 
+        {
+            Target = target
+            BoxedResult =  box callbackResult
+        }
---
+++ src/Dash.NET/Operators.fs
@@ -0,0 +1,9 @@
+﻿namespace Dash.NET
+
+module Operators =
+
+    let inline (.@) (componentId:string) (componentProperty:ComponentProperty) =
+        Dependency.create(componentId, componentProperty)
+
+    let inline (=>) (target:Dependency) (callbackResult) = 
+        CallbackResultBinding.bindResult target callbackResult
