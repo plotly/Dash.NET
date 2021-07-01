af2d65c128057f58eb8736b9d287b76bcf87fcce
Joseph Roorda <joseph.roorda@prolucid.ca>
2021-07-01 11:13:39 -0400

    Added basic property generation to component generation

--- src/Dash.NET.ComponentGeneration/Component.template.fs
+++ src/Dash.NET.ComponentGeneration/Component.template.fs
@@ -1,13 +1,13 @@
 ﻿//---
-//ComponentName: (*{ComponentName}*)
-//camelCaseComponentName: (*{camelCaseComponentName}*)
-//ComponentChar: (*{ComponentChar}*)
-//ComponentNamespace: (*{ComponentNamespace}*)
-//ComponentType: (*{ComponentType}*)
-//LibraryNamespace: (*{LibraryNamespace}*)
+//ComponentName: {ComponentName}
+//camelCaseComponentName: {camelCaseComponentName}
+//ComponentChar: {ComponentChar}
+//ComponentNamespace: {ComponentNamespace}
+//ComponentType: {ComponentType}
+//LibraryNamespace: {LibraryNamespace}
 //---
 
-namespace (*{LibraryNamespace}*)
+namespace ``{LibraryNamespace}``
 
 open Dash.NET
 open System
@@ -15,67 +15,64 @@ open Plotly.NET
 open ComponentPropTypes
 
 [<RequireQualifiedAccess>]
-module (*{ComponentName}*) =
+module ``{ComponentName}`` =
 
-    type (*{ComponentName}*)Props =
-        | ClassName of string
-        static member toDynamicMemberDef (prop:(*{ComponentName}*)Props) =
+    type ``{ComponentName}Props`` =
+        {ComponentPropDefinitions}
+        static member toDynamicMemberDef (prop:``{ComponentName}Props``) =
             match prop with
-            | ClassName p -> "className", box p
+            {ComponentPropDynamicMemberMap}
             
-    type (*{ComponentName}*)() =
+    type ``{ComponentName}``() =
         inherit DashComponent()
         static member applyMembers
             (
                 id : string,
                 children : seq<DashComponent>,
-                ?ClassName : string,
-                ?Style : DashComponentStyle
+                {ComponentPropArguments}
             ) =
             (
-                fun ((*{ComponentChar}*):(*{ComponentName}*)) -> 
+                fun (``{ComponentChar}``:``{ComponentName}``) -> 
 
                     let props = DashComponentProps()
 
                     id |> DynObj.setValue props "id"
                     children |> DynObj.setValue props "children"
-                    ClassName |> DynObj.setValueOpt props "className"
-                    Style |> DynObj.setValueOpt props "style"
 
-                    DynObj.setValue (*{ComponentChar}*) "namespace" "(*{ComponentNamespace}*)"
-                    DynObj.setValue (*{ComponentChar}*) "props" props
-                    DynObj.setValue (*{ComponentChar}*) "type" "(*{ComponentType}*)"
+                    {ComponentPropSetValueMap}
 
-                    (*{ComponentChar}*)
+                    DynObj.setValue ``{ComponentChar}`` "namespace" @"{ComponentNamespace}"
+                    DynObj.setValue ``{ComponentChar}`` "props" props
+                    DynObj.setValue ``{ComponentChar}`` "type" @"{ComponentType}"
+
+                    ``{ComponentChar}``
 
             )
         static member init 
             (
                 id,
                 children,
-                ?ClassName,
-                ?Style
+                {ComponentPropArguments}
             ) = 
-                (*{ComponentName}*)()
-                |> (*{ComponentName}*).applyMembers 
+                ``{ComponentName}``()
+                |> ``{ComponentName}``.applyMembers 
                     (
                         id,
                         children,
-                        ?ClassName = ClassName,
-                        ?Style = Style
+                        {ComponentPropArgumentsMap}
                     )
 
-    let (*{camelCaseComponentName}*) (id:string) (props:seq<(*{ComponentName}*)Props>) (children:seq<DashComponent>) =
-        let (*{ComponentChar}*) = (*{ComponentName}*).init(id,children)
+    let ``{camelCaseComponentName}`` (id:string) (props:seq<``{ComponentName}Props``>) (children:seq<DashComponent>) =
+        let ``{ComponentChar}`` = ``{ComponentName}``.init(id,children)
         let componentProps = 
-            match ((*{ComponentChar}*).TryGetTypedValue<DashComponentProps>("props")) with
+            match (``{ComponentChar}``.TryGetTypedValue<DashComponentProps>("props")) with
             | Some p -> p
             | None -> DashComponentProps()
         props
         |> Seq.iter (fun prop ->
-            let fieldName,boxedProp = prop |> (*{ComponentName}*)Props.toDynamicMemberDef
+            let fieldName,boxedProp = prop |> ``{ComponentName}Props``.toDynamicMemberDef
             boxedProp |> DynObj.setValue componentProps fieldName
         )
-        componentProps |> DynObj.setValue (*{ComponentChar}*) "props" 
-        (*{ComponentChar}*) :> DashComponent
+        componentProps |> DynObj.setValue ``{ComponentChar}`` "props" 
+        ``{ComponentChar}`` :> DashComponent
 
