b721c28b950de934723367047c66b20b57203195
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2021-09-15 16:22:19 -0400

    add interactive callback to the sample app

--- src/Dash.NET.Suave.Example/Program.fs
+++ src/Dash.NET.Suave.Example/Program.fs
@@ -5,6 +5,7 @@ open Dash.NET.Suave
 open Dash.NET.Html
 open Dash.NET.DCC
 open Plotly.NET
+open Dash.NET
 
 let myGraph = Chart.Line([(1,1);(2,2)])
 
@@ -15,16 +16,28 @@ let myLayout =
           Attr.children "Hello world from Dash.NET and Suave!"
       ];
       Html.h2 [ Attr.children "Take a look at this graph:"];
-      Graph.graph "my-ghraph-id" [Graph.Attr.figure(GenericChart.toFigure myGraph)]
+      Graph.graph "my-ghraph-id" [Graph.Attr.figure(GenericChart.toFigure myGraph)];
+      Html.h2 [Attr.children "Tell us something!"];
+      Input.input "test-input" [Input.Attr.inputType InputType.Text];
+      Html.h3 [Attr.children "Input below will not trigger the callback"]
+      Input.input "test-input-state" [Input.Attr.inputType InputType.Text]
+      Html.div [ Attr.id "test-output" ];
     ]
   ]
 
-[<EntryPoint>]
-let main argv =
+let testCallback =
+  Callback.singleOut(
+      "test-input" @. Value,
+      "test-output" @. Children,
+      (fun (input:string) (state:string) ->
+          "test-output" @. Children => (
+              sprintf "You said : '%s' and we added the state: '%s'" input state)
+      ),
+      State = ["test-input-state" @. Value])
 
-  let dashApp =
-    DashApp.initDefault()
-    |> DashApp.withLayout myLayout
+let dashApp =
+  DashApp.initDefault()
+  |> DashApp.withLayout myLayout
+  |> DashApp.addCallback testCallback
 
-  DashApp.run [||] dashApp
-  0
+DashApp.run [||] dashApp
