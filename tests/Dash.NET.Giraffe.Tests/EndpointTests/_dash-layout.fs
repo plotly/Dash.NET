a3dd4d389d1c1a946237e75094a618510a27aa8f
Kevin Schneider <schneider.kev@outlook.de>
2021-10-05 09:07:08 +0200

    add endpoint test for the giraffe sample layout

--- tests/Dash.NET.Giraffe.Tests/EndpointTests/_dash-layout.fs
+++ tests/Dash.NET.Giraffe.Tests/EndpointTests/_dash-layout.fs
@@ -26,5 +26,27 @@ let endpointTests =
             "DCC Graph component" 
             "Incorrect layout JSON"
             """{"namespace":"dash_core_components","props":{"id":"test-graph","children":[],"figure":{"data":[{"type":"scatter","mode":"markers","x":[1],"y":[2]}],"frames":[]}},"type":"Graph"}"""
-
+        
+        Html.div [
+            Attr.children [
+                Html.h1 [ Attr.children "Dropdown Component Test:" ];
+                Dropdown.dropdown "dropdown" [
+                    Dropdown.Attr.loadingState (LoadingState.init(true))
+                    Dropdown.Attr.options [
+                        DropdownOption.init ("1", 1, false, "3")
+                        DropdownOption.init ("2", 2, true)
+                        DropdownOption.init ("3", 3)
+                    ]
+                ]
+                Html.h1 [ Attr.children "Component Test:" ];
+                Html.h1 [ Attr.children "Component Test:" ];
+                Html.h1 [ Attr.children "Component Test:" ];
+                Html.h1 [ Attr.children "Component Test:" ];
+                Html.h1 [ Attr.children "Component Test:" ];
+            ]
+        ]
+        |> generateLayoutEndpointTest 
+            "sample layout" 
+            "Incorrect layout JSON"
+            """{"namespace":"dash_html_components","props":{"children":[{"namespace":"dash_html_components","props":{"children":["Dropdown Component Test:"]},"type":"H1"},{"namespace":"dash_core_components","props":{"id":"dropdown","children":[],"options":[{"label":"1","value":1,"disabled":false,"title":"3"},{"label":"2","value":2,"disabled":true},{"label":"3","value":3}],"loading_state":{"isLoading":true}},"type":"Dropdown"},{"namespace":"dash_html_components","props":{"children":["Component Test:"]},"type":"H1"},{"namespace":"dash_html_components","props":{"children":["Component Test:"]},"type":"H1"},{"namespace":"dash_html_components","props":{"children":["Component Test:"]},"type":"H1"},{"namespace":"dash_html_components","props":{"children":["Component Test:"]},"type":"H1"},{"namespace":"dash_html_components","props":{"children":["Component Test:"]},"type":"H1"}]},"type":"Div"}"""
     ]
