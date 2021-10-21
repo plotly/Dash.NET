0168a9ae39715c4ee541c64133768a919d8cfa90
Juan Vera <juancho_2ce@hotmail.com>
2021-10-21 12:52:09 -0500

    Advances on the Bubble chart code for the Layout doc

--- Dash.NET.Giraffe.CSharp.Tests/Program.cs
+++ Dash.NET.Giraffe.CSharp.Tests/Program.cs
@@ -195,8 +195,8 @@ namespace Dash.Giraffe.CSharp.Example.Tests
 }
 */
 //-----------------------------------------------------------------------
-//getting_started_markdown
-
+//getting_started_table
+/*
 using System;
 using System.Net;
 using System.IO;
@@ -262,6 +262,102 @@ namespace Dash.Giraffe.CSharp.Example.Tests
                             )
                         )
                     )
+                );
+
+            var dashApp = DashApp
+                .initDefault()
+                .withLayout(layout);
+
+            var config = new DashGiraffeConfig(
+                hostName: "localhost",
+                logLevel: LogLevel.Debug,
+                ipAddress: "*",
+                port: 8000,
+                errorHandler: (Exception err) => Core.text(err.Message)
+            );
+
+            dashApp.run(
+                args: new string[] { },
+                config
+            );
+        }
+    }
+}
+*/
+//example getting_started_viz
+//-----------------------------------------------------------------------
+using System;
+using System.Net;
+using System.IO;
+using Dash.NET.CSharp.DCC;
+using Plotly.NET;
+using Giraffe;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using static Dash.NET.CSharp.Dsl;
+using Dash.NET.CSharp.Giraffe;
+using System.Linq;
+using System.Globalization;
+
+namespace Dash.Giraffe.CSharp.Example.Tests
+{
+    class Program
+    {
+        public static string GetCSV(string url)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            var resp = (HttpWebResponse)req.GetResponse();
+
+            var sr = new StreamReader(resp.GetResponseStream());
+            var results = sr.ReadToEnd();
+            sr.Close();
+
+            return results;
+        }
+        static void Main(string[] args)
+        {
+
+            List<string> splitted = new List<string>();
+            var csv = GetCSV("https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv");
+            var headers = csv
+                .Split("\n")
+                .First()
+                .Split(",");
+            var rows = csv
+                .Split("\n")
+                .Skip(1)
+                .SkipLast(1)
+                .Select(x => x.Split('"'))
+                .ToList();
+            //Console.WriteLine(rows[0][3]);
+            foreach (var item in rows)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.Write(item2 + " ");
+                }
+                Console.Write("\n");
+
+            }
+            //Console.WriteLine(rows[3][0]);
+            //var testList = rows.Select(x => x[3]).ToList();
+            //foreach (var item in testList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //var fig = Plotly.NET.Chart2D.Chart.Bubble<decimal, decimal, decimal>(
+            //    x: rows.Select(x => decimal.Parse(x[4])),
+            //    y: rows.Select(x => decimal.Parse(x[5])),
+            //    sizes: rows.Select(x => int.Parse(x[3])));
+            var layout =
+            Html.div(
+                //Attr.children(
+                //    Graph.graph(
+                //            "my-graph",
+                //            Graph.Attr.figure(GenericChart.toFigure(fig))
+                //            )
+
+                //    )
                 );
 
             var dashApp = DashApp
