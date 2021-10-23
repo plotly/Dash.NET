fab2eca149ba9a694f9423cc3de8fbf3e49e64a5
Juan Vera <juancho_2ce@hotmail.com>
2021-10-22 19:25:40 -0500

    Advances on the bubble chart example.

--- Dash.NET.Giraffe.CSharp.Tests/Layout_Viz.cs
+++ Dash.NET.Giraffe.CSharp.Tests/Layout_Viz.cs
@@ -39,13 +39,42 @@ namespace Documentation.Examples
             var csvUrl = "https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv";
             var csv = new StreamReader(new HttpClient().GetStreamAsync(csvUrl).Result);
             var rows = new CsvReader(csv, CultureInfo.InvariantCulture).GetRecords<LifeExpectancyGdp>().ToList();
-            
-            Console.WriteLine(rows);
+            var labels = rows.Select(x => "<br>continent="+x.continent+"<br>gdp per capita="+x.gdp_per_capita+"<br>life expectancy="+x.life_expectancy+"<br>population="+x.population).ToList();
+            //Are colors from the Dash Design Kit? there's no way to auto generate them.
+            var colors = rows.Select(x =>
+                {
+                    if (x.continent == "Asia")
+                        return Color.fromHex("#119DFF");
+                    else if (x.continent == "Europe")
+                        return Color.fromHex("#54CD71");
+                    else
+                        return Color.fromHex("#809723");
+                }
+            );
 
+            var chart = Chart2D.Chart.Bubble<decimal, decimal, string>(
+                    x: rows.Select(x => x.gdp_per_capita),
+                    y: rows.Select(x => x.life_expectancy),
+                    sizes: rows.Select(x => Decimal.ToInt32(x.population / 10000000)),
+                    Labels: labels,
+                    ShowLegend: true,
+                    Name: "Asia" //This fixes the legend name, but do we need to make a chart for every continent with different names to use the legend functions?
+                    //Color: Color.fromColors(colors)
+                )
+                .WithXAxis(
+                    Plotly.NET.LayoutObjects.LinearAxis.init<int, int, int, int, int, int>(AxisType: StyleParam.AxisType.Log, Title: Plotly.NET.Title.init("Gdp per capita"))
+                )
+                .WithYAxis(
+                    Plotly.NET.LayoutObjects.LinearAxis.init<int, int, int, int, int, int>(Title: Plotly.NET.Title.init("Life expectancy"))
+                );
+            
+            var fig = GenericChart.toFigure(chart);
 
             var layout =
                     Html.div(
-
+                        Attr.children(
+                            Graph.graph("life-exp-vs-gdp", Graph.Attr.figure(fig))
+                        )
                     );
 
             var dashApp = DashApp
